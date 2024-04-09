b4f9bbb2e8004ff71b4958f621e1475f517517f5
Miski <miskiyusuf@hotmail.com>
2024-04-10 01:24:12 +0200

    responsiv

--- src/pages/Hjem.jsx
+++ src/pages/Hjem.jsx
@@ -146,16 +146,16 @@ function Møtoss({ language }) {
     <>
             <div className="kontaktOssBoks">
                 <div className="kontaktOssHeader">
-                    <h1>{textData.meet_us }</h1>
+                    <h2>{textData.meet_us }</h2>
                 </div>
                 <div className="kontaktOssKort">
                         <img className="kontaktOssKort-img" src="./Moosa.jpg" alt="Bilde av Moosa, en av grunnleggerne av M&N Regnskap (Statsautorisert regnskapsfører)" />
-                        <h1>MOOSA ALI RASHID</h1>
+                        <h2>MOOSA ALI RASHID</h2>
                         <p>{textData.charted_accountant}</p>
                 </div>
                 <div className="kontaktOssKort">
                         <img className="kontaktOssKort-img" src="./Naila.png" alt="Bilde av Naila, en av grunnleggerne av M&N Regnskap (Statsautorisert regnskapsfører)" />
-                        <h1>NAILA SOHAIL KHOKHAR</h1>
+                        <h2>NAILA SOHAIL KHOKHAR</h2>
                         <p>{textData.charted_accountant}</p>
                 </div>
             </div>
