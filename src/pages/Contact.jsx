d57b6a988ec8d171a3501bc3954f52236886cf7f
ShagunRaturi <s358981@oslomet.no>
2024-04-10 17:40:51 +0200

    google api

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -91,7 +91,14 @@ function Contact() {
                         <div className="connect-main">
                             <div className="connect-body">
                                 <div className="map-box">
-                                    <img src="./map.png" alt="Global Offices Map" />
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.231473078393!2d10.841315276934159!3d59.92830116293287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416dd150a3273b%3A0xeb0005b15b49be33!2sM%20%26%20N%20Regnskap%20DA!5e0!3m2!1sno!2sno!4v1712682261082!5m2!1sno!2sno"
+                                        width="600"
+                                        height="450"
+                                        style={{ border: "0" }}
+                                        allowfullscreen=""
+                                        loading="lazy"
+                                    ></iframe>
                                 </div>
                                 <div className="connect-detail">
                                     <div className="detail-item">
