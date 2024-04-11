b32126a9a53af3d30cc18a1d4d5577a132a3a950
Miski <miskiyusuf@hotmail.com>
2024-04-11 21:14:10 +0200

    prøver å fikse router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './routes/root'; // Juster import-stien etter behov
+import Layout from './routes/root'; // Pass på at stien til denne importen er korrekt
 import HomePage from './pages/Home';
 import LinkPage from './pages/Link';
 import TeamPage from './pages/Team';
 import ContactPage from './pages/Contact';
 
+// Sett basename basert på miljøvariabel eller hardkodet, hvis miljøvariabel ikke er tilgjengelig
+const basename = process.env.PUBLIC_URL || '/MN_Regnskap';
+
 function App() {
     return (
-        <Router>
+        <Router basename={basename}>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
                     <Route path="hjem" element={<HomePage />} />
                     <Route path="link" element={<LinkPage />} />
-                    <Route path="/MN_Regnskap/om-oss" element={<TeamPage />} />
+                    <Route path="om-oss" element={<TeamPage />} />
                     <Route path="contact" element={<ContactPage />} />
                 </Route>
             </Routes>
