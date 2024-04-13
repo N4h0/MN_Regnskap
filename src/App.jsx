1ccbd119787d02dfb3e14b13702e7b0d3ab15505
ShagunRaturi <s358981@oslomet.no>
2024-04-13 22:28:34 +0200

    får ikke pulle før jeg commiter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './routes/root'; // Pass på at stien til denne importen er korrekt
+import Layout from './routes/root'; // Juster import-stien etter behov
 import HomePage from './pages/Home';
 import LinkPage from './pages/Link';
 import TeamPage from './pages/Team';
 import ContactPage from './pages/Contact';
 
-// Sett basename basert på miljøvariabel eller hardkodet, hvis miljøvariabel ikke er tilgjengelig
-const basename = process.env.PUBLIC_URL || '/MN_Regnskap';
-
 function App() {
     return (
-        <Router basename={basename}>
+        <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
                     <Route path="hjem" element={<HomePage />} />
                     <Route path="link" element={<LinkPage />} />
-                    <Route path="om-oss" element={<TeamPage />} />
+                    <Route path="team" element={<TeamPage />} />
                     <Route path="contact" element={<ContactPage />} />
                 </Route>
             </Routes>
