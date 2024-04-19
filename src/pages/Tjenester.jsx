4808e38fa8ef9e7b9943dda3d74bacd8040493b6
AmnaDastgir <s364520@oslomet.no>
2024-04-19 19:55:29 +0200

    tjenester pages

---
+++ src/pages/Tjenester.jsx
@@ -0,0 +1,10 @@
+import './Tjenester.css';
+import en from '../languages/en.json'; // Engelsk språkdata
+import no from '../languages/no.json'; // Norsk språkdata
+import { useContext, useEffect } from 'react';
+import { LanguageContext } from '../languages/LanguageContext';
+
+
+
+
+export default Tjenester;
