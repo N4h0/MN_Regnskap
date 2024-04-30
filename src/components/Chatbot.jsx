0e118916818ab1ebf29004f14ebb2cfa6894ec2e
N4h0 <89570972+N4h0@users.noreply.github.com>
2024-05-01 01:03:28 +0200

    Uten CoSim-score.

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -238,7 +238,8 @@ function ChatFooter({ onSend, setShowSuggestions }) {
 
     // Deretter sender hvert av forslagene som separate meldinger
     data.suggestions.forEach((suggestion) => {
-      const suggestionMessage = `${suggestion.question} (Likhet: ${suggestion.CoSim.toFixed(2)})`;
+        const suggestionMessage = suggestion.question;
+        setShowSuggestions(true);
       onSend(suggestionMessage, "suggestion"); // Send meldingen med riktig type "suggestion"
       setShowSuggestions(true);
             });
